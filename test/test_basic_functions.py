8e0479c5d02663b783c3a9e9af62bcd8314c1fbd
HeinDuijf <duijf.hein@gmail.com>
2023-01-13 21:00:30 +0100

    added test_convert_math_to_text

--- test/test_basic_functions.py
+++ test/test_basic_functions.py
@@ -1,5 +1,5 @@
 import config as cfg
-from basic_functions import majority_winner
+from basic_functions import convert_math_to_text, majority_winner
 
 # def test_most_frequent():
 #     values = ["a", "b", "c", "a", "b"]
@@ -26,3 +26,15 @@ def test_majority_winner():
     ]
     result = majority_winner(values)
     assert result == cfg.vote_for_elites or result == cfg.vote_for_mass
+
+
+def test_convert_math_to_text():
+    assert convert_math_to_text("p_e") == "minority_competence"
+    assert convert_math_to_text("p_m") == "majority_competence"
+    assert convert_math_to_text("E") == "number_of_minority"
+    assert convert_math_to_text("I_e") == "influence_minority_proportion"
+    assert convert_math_to_text("h") == "homophily"
+    assert convert_math_to_text("p_e + p_m") == [
+        "minority_competence",
+        "majority_competence",
+    ]
