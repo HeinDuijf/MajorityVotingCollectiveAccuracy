0ccf9fcbbeb2b67fa8057054fec3e434944daabe
HeinDuijf <duijf.hein@gmail.com>
2023-01-19 11:19:11 +0100

    changed back from json to pickle

--- save_read_community.py
+++ save_read_community.py
@@ -1,51 +1,64 @@
-import json
 import os
+import pickle
 
 from community import Community
 
 
-def edges_compress(edges: list):
-    edges = list(edges)
+def community_compress(community: Community):
     edges_dict = {}
-    for source, target in edges:
+    for source, target in community.network.edges():
         if source not in edges_dict.keys():
-            edges_dict[source] = [target]
+            edges_dict[source] = f"{target}"
         else:
-            edges_dict[source].append(target)
-    return edges_dict
+            edges_dict[source] = f"{edges_dict[source]},{target}"
+    community_dict = {
+        "N": community.number_of_nodes,
+        "E": community.number_of_elites,
+        "d": community.degree,
+        "pe": community.elite_competence,
+        "pm": community.mass_competence,
+        "pp": community.probability_preferential_attachment,
+        "h": community.probability_homophilic_attachment,
+        **edges_dict,
+    }
+    return community_dict
 
 
-def edges_unpack(edges_dict: dict):
-    edges_dict = dict(edges_dict)
-    edges = [
-        (source, target)
-        for source in edges_dict.keys()
-        for target in edges_dict[source]
+def community_unpack(community_compressed: dict):
+    nodes = [
+        node
+        for node in range(community_compressed["N"])
+        if node in community_compressed.keys()
     ]
-    return edges
+    edges = [
+        (source, int(target))
+        for source in nodes
+        for target in community_compressed[source].split(",")
+    ]
+    community_dict = {
+        "number_of_nodes": community_compressed["N"],
+        "number_of_elites": community_compressed["E"],
+        "degree": community_compressed["d"],
+        "elite_competence": community_compressed["pe"],
+        "mass_competence": community_compressed["pm"],
+        "probability_preferential_attachment": community_compressed["pp"],
+        "probability_homophilic_attachment": community_compressed["h"],
+        "edges": edges,
+    }
+    community = Community(**community_dict)
+    return community
 
 
 def save_community_to_file(filename: str, community: Community):
     path = os.path.dirname(filename).replace("\\", "/")
     os.makedirs(path, exist_ok=True)
-    community_dict: dict = {
-        "number_of_nodes": community.number_of_nodes,
-        "number_of_elites": community.number_of_elites,
-        "degree": community.degree,
-        "elite_competence": community.elite_competence,
-        "mass_competence": community.mass_competence,
-        "probability_preferential_attachment": community.probability_preferential_attachment,
-        "probability_homophilic_attachment": community.probability_homophilic_attachment,
-        "edges": edges_compress(community.network.edges()),
-    }
-    with open(f"{filename}", "wb") as f:
-        json.dump(community_dict, f, protocol=-1)
+    community_compressed = community_compress(community)
+    with open(f"{filename}.pickle", "wb") as f:
+        pickle.dump(community_compressed, f)
 
 
 def read_community_from_file(filename: str):
-    with open(f"{filename}", "rb") as f:
-        community_dict = json.load(f)
-    edges = edges_unpack(community_dict["edges"])
-    community_dict["edges"] = edges
-    community = Community(**community_dict)
+    with open(f"{filename}.pickle", "rb") as f:
+        community_compressed = pickle.load(f)
+    community = community_unpack(community_compressed)
     return community
