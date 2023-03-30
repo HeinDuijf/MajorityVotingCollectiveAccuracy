722334bb32f4b31ea759ac54016f92b294777e40
HeinDuijf <duijf.hein@gmail.com>
2023-03-30 13:49:47 +0200

    some notebook updates

--- notebook.py
+++ notebook.py
@@ -12,33 +12,32 @@ mass_color = "#9C27B0"
 
 
 def set_node_attributes(com: Community, color_type: str = "type"):
-    if color_type == "type":
-        for node in com.nodes:
-            if node in com.nodes_elite:
-                com.network.nodes[node]["color"] = elite_color
-            else:
-                com.network.nodes[node]["color"] = mass_color
-    elif color_type == "opinion":
-        for node in com.nodes:
-            if com.network.nodes[node]["opinion"] == vote_for_elites:
-                com.network.nodes[node]["color"] = elite_color
-            else:
-                com.network.nodes[node]["color"] = mass_color
-    elif color_type == "vote":
-        for node in com.nodes:
-            if com.network.nodes[node]["vote"] == vote_for_elites:
-                com.network.nodes[node]["color"] = elite_color
-            else:
-                com.network.nodes[node]["color"] = mass_color
+    def translate_to_color(symbol):
+        if symbol == vote_for_elites or symbol == "elite":
+            return elite_color
+        else:
+            return mass_color
+
     for node in com.nodes:
         com.network.nodes[node]["label"] = str(node)
         com.network.nodes[node]["size"] = com.network.in_degree(node) + 1
         com.network.nodes[node]["level"] = com.network.in_degree(node)
+        if color_type == "type":
+            com.network.nodes[node]["color"] = translate_to_color(
+                com.network.nodes[node]["type"]
+            )
+        elif color_type == "opinion":
+            com.network.nodes[node]["color"] = translate_to_color(
+                com.network.nodes[node]["opinion"]
+            )
+        elif color_type == "vote":
+            com.network.nodes[node]["color"] = translate_to_color(
+                com.network.nodes[node]["vote"]
+            )
 
 
 def visualize(com: Community, color_type="type"):
     set_node_attributes(com=com, color_type=color_type)
-    nx_net = com.network
     nt = Network(
         height="500px",
         width="100%",
@@ -48,7 +47,24 @@ def visualize(com: Community, color_type="type"):
         cdn_resources="remote",
     )
     nt.set_edge_smooth("curvedCCW")
-    nt.from_nx(nx_net)
+    for node in com.nodes_elite:
+        nt.add_node(node, x=-1000, **com.network.nodes[node])
+    for node in com.nodes_mass:
+        nt.add_node(node, x=1000, **com.network.nodes[node])
+
+    hide_edges = False
+    if color_type == "vote":
+        hide_edges = True
+    elif color_type == "type":
+        nt.inherit_edge_colors(False)
+
+    for edge in com.network.edges():
+        nt.add_edge(
+            edge[0],
+            edge[1],
+            hidden=hide_edges,
+            color=com.network.nodes[edge[1]]["color"],
+        )
     nt.hrepulsion(node_distance=200, damping=0.4)
     nt.prep_notebook()
     return nt
