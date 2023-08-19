45f63b169db02fc36ae1d1a947f88b305a68ea1c
HeinDuijf <duijf.hein@gmail.com>
2023-08-19 08:24:08 +0200

    added figure of distribution accuracy prior to influence

--- figures.py
+++ figures.py
@@ -1,15 +1,13 @@
 import os
 
-from generate_figures.figure_accuracy_homophily import \
-    figure_accuracy_homophilic
-from generate_figures.figure_distribution_accuracy import \
-    figure_distribution_accuracy
-from generate_figures.figure_distribution_in_degree import \
-    figure_distribution_in_degree
-from generate_figures.figure_distribution_influence import \
-    figure_distribution_influence
-from generate_figures.figure_epistemic_accuracy import \
-    figure_epistemic_accuracy
+from generate_figures.figure_accuracy_homophily import figure_accuracy_homophilic
+from generate_figures.figure_distribution_accuracy import figure_distribution_accuracy
+from generate_figures.figure_distribution_accuracy_pre_influence import (
+    figure_distribution_accuracy_pre_influence,
+)
+from generate_figures.figure_distribution_in_degree import figure_distribution_in_degree
+from generate_figures.figure_distribution_influence import figure_distribution_influence
+from generate_figures.figure_epistemic_accuracy import figure_epistemic_accuracy
 
 if __name__ == "__main__":
     folder_name = "new_figures"
@@ -20,6 +18,11 @@ if __name__ == "__main__":
     #     number_of_communities=200,
     #     number_of_voting_simulations=200,
     # )
+    figure_distribution_accuracy_pre_influence(
+        filename=f"{folder_name}/figure_distribution_accuracy_pre_influence",
+        data_file="data/clean.csv",
+    )
+
     figure_distribution_influence(
         filename=f"{folder_name}/figure_distribution_influence",
         data_file="data/clean.csv",
---
+++ generate_figures/figure_distribution_accuracy_pre_influence.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+
+from generate_figures.figure_basics import histogram_plot
+
+
+def figure_distribution_accuracy_pre_influence(
+    filename: str = None, data_file: str = "data/clean.csv"
+):
+    """Plots the distribution of the collective accuracy prior to social influence and
+    the cumulative line plot.
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+        Plot of the distribution of the collective accuracy prior to social
+        influence and the cumulative line plot."""
+    df = pd.read_csv(data_file)
+
+    # Histogram
+    histogram_plot(
+        filename=filename,
+        dataframe=df,
+        y="collective_accuracy_pre_influence",
+        title="Distribution of collective accuracy",
+        ylabel_left="number of occurrences",
+        xlabel="collective accuracy",
+        xlim=(0, 1),
+        xticks=0.1 * np.arange(0, 11, 1, dtype=int),
+        ylabel_right="cumulative probability",
+        ylim=(0, 1),
+    )
+
+
+if __name__ == "__main__":
+    figure_distribution_accuracy(data_file="../data/test.csv")
