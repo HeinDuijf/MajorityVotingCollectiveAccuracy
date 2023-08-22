57142ff9ed5721b313a544bbb54f93c1605ceced
HeinDuijf <duijf.hein@gmail.com>
2023-08-22 08:58:21 +0200

    added variable data_file

--- statistics.py
+++ statistics.py
@@ -4,9 +4,10 @@ from stats.table_variance import table_variance
 from stats.table_variance_multiple_datasets import table_variance_multiple_datasets
 
 if __name__ == "__main__":
-    table_variance(data_file="data/clean.csv", output_file="table_variance")
+    data_file = "data/clean.csv"
+    table_variance(data_file=data_file, output_file=f"table_variance")
     table_variance_multiple_datasets(
-        data_file="data/clean.csv", output_file="table_variance_multiple"
+        data_file=data_file, output_file=f"table_variance_multiple"
     )
-    table_std_coefficients(data_file="data/clean.csv", output_file="table_std_coeff")
-    table_absolute_change(data_file="data/clean.csv", output_file="table_abs_change")
+    table_std_coefficients(data_file=data_file, output_file=f"table_std_coeff")
+    table_absolute_change(data_file=data_file, output_file=f"table_abs_change")
