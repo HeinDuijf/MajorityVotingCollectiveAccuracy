3be59fcca15607dee970f668e3e5d7e8e356047c
HeinDuijf <duijf.hein@gmail.com>
2023-10-25 11:37:54 +0200

    uncomment Simulation(..)

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import pickle
+
+from scripts.save_read_community import (
+    community_unpack,
+    read_community_from_combined_file,
+)
 from simulation import Simulation
 
 if __name__ == "__main__":
@@ -6,14 +12,14 @@ if __name__ == "__main__":
     number_of_voting_simulations = 10 ** 5
     number_of_nodes = 10 ** 2
 
-    Simulation(
-        filename_csv="data/clean",
-        folder_communities="data/communities",
-        number_of_communities=number_of_communities,
-        number_of_voting_simulations=number_of_voting_simulations,
-        number_of_nodes=number_of_nodes,
-        elite_competence_range=(0.55, 0.7),
-        mass_competence_range=(0.55, 0.7),
-        number_of_elites_range=(25, 45),
-        probability_homophilic_attachment_range=(0.5, 0.75),
-    ).run()
+    # Simulation(
+    #     filename_csv="data/clean",
+    #     folder_communities="data/communities",
+    #     number_of_communities=number_of_communities,
+    #     number_of_voting_simulations=number_of_voting_simulations,
+    #     number_of_nodes=number_of_nodes,
+    #     elite_competence_range=(0.55, 0.7),
+    #     mass_competence_range=(0.55, 0.7),
+    #     number_of_elites_range=(25, 45),
+    #     probability_homophilic_attachment_range=(0.5, 0.75),
+    # ).run()
